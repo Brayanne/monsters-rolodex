032f5aea08912ce629bed547a1087693d97fcd6a
Brayanne Reyes Ron <brayannereyes12@gmail.com>
2020-02-16 12:33:04 -0800

    Updates

---
+++ precache-manifest.a6bf4c98cc38717695a2b946be3ed2bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771253b36b50cc632785e7901e8798c1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7894de4a4495bf2cb7d2",
+    "url": "/monsters-rolodex/static/css/main.7efd5281.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monsters-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7894de4a4495bf2cb7d2",
+    "url": "/monsters-rolodex/static/js/main.cc1545b7.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  }
+]);
